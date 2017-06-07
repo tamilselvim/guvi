643d28683bd382232d58c075a782098af52a7919
kavipriyaL <rajkavin889@gmail.com>
2017-06-07 10:14:14 +0530

    Create Print_hello_world.java

---
+++ Print_hello_world.java
@@ -0,0 +1,11 @@
+import java.util.*;
+public class Print_hello_world {
+        public static void main(String args[])
+{
+ Scanner in=new Scanner(System.in);
+ for(int i=0;i<5;i++)
+ {
+     System.out.println("hello world");
+ }
+}
+}
